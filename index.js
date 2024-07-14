3363ad21b0c68fdf74764ce22a5afa9f4061ace7
oxbryte <silascyrax@gmail.com>
2024-07-14 17:40:47 +0100

    chore: Update welcome message and buttons in index.js

--- index.js
+++ index.js
@@ -8,64 +8,73 @@ const bot = new Telegraf("6795133302:AAH-W3dcRilnc72R6goPied0mkPkpQJ8J9Q");
 const imagePath = (fileName) => path.join(__dirname, "images", fileName);
 
 bot.start((ctx) => {
+  // ctx.reply("Welcome to the bot! Let me show you around.");
+  const message = `<b>Hey ${ctx.from.first_name} Welcome to Loozr!👋 </b> \n
+The First Social Music Layer of Web 3.0 ☘️ | 📀 \n
+Loozr is your Open Web and DeFi hub for creators and musicians.\n
+Invest in talents, tokenize creative assets, trade as cryptocurrencies, and grow with your fans. \n
+Join the Loozr Airdrop, farm Loozr Points (LP), engage, contribute & #Listen2Earn 🎶🤑🔥`;
+
   ctx.replyWithPhoto(
     { source: imagePath("loozr.jpg") },
     {
-      caption: "Welcome!",
+      caption: message,
+      parse_mode: "HTML",
       reply_markup: {
         inline_keyboard: [
           [
             {
-              text: "Open Loozr",
+              text: "📀 Let's go",
               web_app: { url: "https://learning-mini-bot.vercel.app/" },
             },
-            { text: "Join community", callback_data: "loozr.jpg" },
           ],
+          [{ text: "👍 Join Loozr Community", url: "https://example.com/how-to-play" }],
+          [{ text: "🎓 How to play", url: "https://example.com/how-to-play" }],
         ],
       },
     }
   );
 });
 
-bot.action("btn1", (ctx) => {
-  ctx.replyWithPhoto(
-    { source: imagePath("loozr.jpg") },
-    {
-      caption: "You pressed Button 1!",
-      reply_markup: {
-        inline_keyboard: [[{ text: "Back", callback_data: "back" }]],
-      },
-    }
-  );
-});
+// bot.action("btn1", (ctx) => {
+//   ctx.replyWithPhoto(
+//     { source: imagePath("loozr.jpg") },
+//     {
+//       caption: "You pressed Button 1!",
+//       reply_markup: {
+//         inline_keyboard: [[{ text: "Back", callback_data: "back" }]],
+//       },
+//     }
+//   );
+// });
 
-bot.action("btn2", (ctx) => {
-  ctx.replyWithPhoto(
-    { source: imagePath("loozr.jpg") },
-    {
-      caption: "You pressed Button 2!",
-      reply_markup: {
-        inline_keyboard: [[{ text: "Back", callback_data: "back" }]],
-      },
-    }
-  );
-});
+// bot.action("btn2", (ctx) => {
+//   ctx.replyWithPhoto(
+//     { source: imagePath("loozr.jpg") },
+//     {
+//       caption: "You pressed Button 2!",
+//       reply_markup: {
+//         inline_keyboard: [[{ text: "Back", callback_data: "back" }]],
+//       },
+//     }
+//   );
+// });
 
-bot.action("back", (ctx) => {
-  ctx.replyWithPhoto(
-    { source: imagePath("loozr.jpg") },
-    {
-      caption: "Welcome!",
-      reply_markup: {
-        inline_keyboard: [
-          [
-            { text: "Button 1", callback_data: "btn1" },
-            { text: "Button 2", callback_data: "btn2" },
-          ],
-        ],
-      },
-    }
-  );
-});
+// bot.action("back", (ctx) => {
+//   ctx.replyWithPhoto(
+//     { source: imagePath("loozr.jpg") },
+//     {
+//       caption: "Welcome!",
+//       reply_markup: {
+//         inline_keyboard: [
+//           [
+//             { text: "Button 1", callback_data: "btn1" },
+//             { text: "Button 2", callback_data: "btn2" },
+//           ],
+//         ],
+//       },
+//     }
+//   );
+// });
 
 bot.launch();
